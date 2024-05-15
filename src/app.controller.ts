cde0415a913c429c53ade13f689d7c126f4a1bf9
Ray lyu <mlxglyuray003@gmail.com>
2024-05-15 11:10:53 +0800

    feat:路由-认识request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 // Controller 中可以添加统一后缀
@@ -8,7 +9,8 @@ export class AppController {
 
   // 请求完整url => api/user
   @Get('/user')
-  getHello (): string {
+  getHello (@Req() req: Request): string {
+    console.log('浏览器发送的请求', req.url)
     return this.appService.getHello();
   }
 
