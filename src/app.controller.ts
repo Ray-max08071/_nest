474d7388a152ad75064e17b17d0b6e5ee10ef166
Ray lyu <mlxglyuray003@gmail.com>
2024-05-15 10:57:02 +0800

    feat:路由-01

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// Controller 中可以添加统一后缀
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor (private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
+  // 请求完整url => api/user
+  @Get('/user')
+  getHello (): string {
     return this.appService.getHello();
+  }
+
+  // 请求完整url => api/name
+  @Get('/name')
+  getName (): string {
+    return 'name';
   }
 }
