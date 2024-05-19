978da1d1fb2f929123a3561981a8163125c7ad3a
Ray lyu <mlxglyuray003@gmail.com>
2024-05-19 20:43:29 +0800

    feat:全局中间件注册

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DemoMiddleware } from './middleware/demo.middleware';
 
-async function bootstrap() {
+async function bootstrap () {
   const app = await NestFactory.create(AppModule);
+  // 使用app.use 可以注册全局中间件
+  app.use(DemoMiddleware)
   await app.listen(3000);
 }
 bootstrap();
